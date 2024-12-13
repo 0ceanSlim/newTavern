7861ee7a87b78d76124a0d1107d6a5463c5f41cf
Chris kerr <chriskerr@gmx.com>
2024-12-12 21:43:22 -0500

    init routes function

--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	//"goFrame/src/handlers"
+	"fmt"
 	"goFrame/src/routes"
 	"goFrame/src/utils"
-
-	"fmt"
 	"net/http"
 )
 
@@ -20,16 +18,7 @@ func main() {
 	mux := http.NewServeMux()
 
 	// Initialize Routes
-	mux.HandleFunc("/", routes.Index)
-	mux.HandleFunc("/settings", routes.Settings)
-	mux.HandleFunc("/what-is-nostr", routes.WhatIsNostr)
-	mux.HandleFunc("/bitcoin-works", routes.BitcoinWorks)
-	mux.HandleFunc("/mempool", routes.Mempool)
-	mux.HandleFunc("/nostr-clients", routes.NostrClients)
-	mux.HandleFunc("/nostr-mobile", routes.NostrMobile)
-	mux.HandleFunc("/nostr-mobile-android", routes.NostrMobileAndroid)
-	mux.HandleFunc("/nostr-mobile-ios", routes.NostrMobileIos)
-	mux.HandleFunc("/nostr-desktop", routes.NostrDesktop)
+	routes.InitializeRoutes(mux)
 
 	fmt.Printf("Server is running on http://localhost:%d\n", cfg.Port)
 	http.ListenAndServe(fmt.Sprintf(":%d", cfg.Port), mux)
---
+++ src/routes/InitializeRoutes.go
@@ -0,0 +1,16 @@
+package routes
+
+import "net/http"
+
+func InitializeRoutes(mux *http.ServeMux) {
+	mux.HandleFunc("/", Index)
+	mux.HandleFunc("/settings", Settings)
+	mux.HandleFunc("/what-is-nostr", WhatIsNostr)
+	mux.HandleFunc("/bitcoin-works", BitcoinWorks)
+	mux.HandleFunc("/mempool", Mempool)
+	mux.HandleFunc("/nostr-clients", NostrClients)
+	mux.HandleFunc("/nostr-mobile", NostrMobile)
+	mux.HandleFunc("/nostr-mobile-android", NostrMobileAndroid)
+	mux.HandleFunc("/nostr-mobile-ios", NostrMobileIos)
+	mux.HandleFunc("/nostr-desktop", NostrDesktop)
+}
