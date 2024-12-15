a1d5b15657bfe3302822f5a4b769d70d670ed8db
Chris kerr <chriskerr@gmx.com>
2024-12-15 08:20:33 -0500

    move wellknown and videos to we root

--- src/utils/serveWellKnownNostr.go
+++ src/utils/serveWellKnownNostr.go
@@ -4,5 +4,5 @@ import "net/http"
 
 func ServeWellKnownNostr(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
-	http.ServeFile(w, r, "web/static/.well-known/nostr.json")
+	http.ServeFile(w, r, "web/.well-known/nostr.json")
 }
